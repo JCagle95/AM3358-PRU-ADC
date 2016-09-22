51bf20c9bd51c851ebd0f9c200656fc21717e371
Jackson Cagle <jackson.cagle@gmail.com>
2016-09-22 19:25:06 -0400

    RAM Writing . Input control

--- PRURAM_V1/RAMWriting.c
+++ PRURAM_V1/RAMWriting.c
@@ -35,7 +35,7 @@ typedef struct {
 } app_data;
 
 app_data info;
-int workthread_running = 1;
+int workthread_running = 0;
 
 void intHandler(int dummy) {
     printf("Setting run flag to 0\n");
@@ -44,11 +44,32 @@ void intHandler(int dummy) {
 
 void* pru_ram_task(void *arg){
 	
-	info.pru_data->data_size = ARRAY_SIZE * sizeof(uint32_t);
-	int Counter = 0;
+	unsigned int ret;
+	// Initialize PRU0
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+	prussdrv_init();
+	ret = prussdrv_open(PRU_EVTOUT_0);
+	if (ret)
+	{
+		printf("prussdrv_open failed\n");
+	}
 
+	// Initialize Interrupt
+	prussdrv_pruintc_init(&pruss_intc_initdata);
+
+	// Initialize Memory Mapping for local memory
+	prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, (void *) &info.pru_memory);
+	info.pru_data = info.pru_memory;
+
+	// Initialize Parameters
+	info.pru_data->run_flag = 1;
+	info.pru_data->data_ready = 0;
+	info.pru_data->data_size = ARRAY_SIZE * sizeof(uint32_t);
+
+	prussdrv_exec_program (PRU_NUM0, "./ram_writing.bin");
+
+	int Counter = 0, pointer = 0;
 	uint32_t dataPoint[100000];
-	int pointer = 0;
 	FILE * pFile;
 	while(info.pru_data->run_flag){
 		if (pointer == 0){
@@ -82,56 +103,67 @@ void* pru_ram_task(void *arg){
 		}
 		fflush(stdout);
 	}
+
 	workthread_running = 0;
+
+	// Wait for PRU0 to halt
+	prussdrv_pru_wait_event(PRU_EVTOUT_0);
+	prussdrv_pru_clear_event(PRU0_ARM_INTERRUPT);
+
+	// Safe Quit PRUs
+	prussdrv_pru_disable(PRU_NUM0);
+	prussdrv_exit();
+
 	return NULL;
 }
 
 int main (void)
 {
-	unsigned int ret;
 	pthread_t tid;
-	
-	// Initialize PRU0
-	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-	prussdrv_init();
-	ret = prussdrv_open(PRU_EVTOUT_0);
-	if (ret)
-	{
-		printf("prussdrv_open failed\n");
-		return ret;
-	}
-	
-	// Initialize Interrupt
-	prussdrv_pruintc_init(&pruss_intc_initdata);
-	
-	// Initialize Memory Mapping for local memory
-	prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, (void *) &info.pru_memory);
-	info.pru_data = info.pru_memory;
-	
-	// Initialize Parameters
-	info.pru_data->run_flag = 1;
-	info.pru_data->data_ready = 0;
 	
 	// This is Thread-Safe Halting
 	signal (SIGQUIT, intHandler);
 	signal (SIGINT, intHandler);
 	
-	// Thread Creation
-	pthread_create(&tid, NULL, &pru_ram_task, NULL);
-    prussdrv_exec_program (PRU_NUM0, "./ram_writing.bin");
+	int taskContinue = 1;
+	int input;
+	while (taskContinue)
+	{
+		printf("1: Task Start, 2: Task End, 3: QUIT\nPlease enter: ");
+		scanf("%d",&input);
+		printf("\n");
 
-	printf("Press ctrl-c to stop running\n\n");
-    while(workthread_running) {
-        sleepms(250);
-    }
+		switch(input){
+			case 1:
+				if (!workthread_running){
+					workthread_running = 1;
+					pthread_create(&tid, NULL, &pru_ram_task, NULL);
+				} else {
+					printf("The task is already running. Please enter 2 to stop PRU\n");
+				}
+				break;
+
+			case 2:
+				if (workthread_running){
+				    info.pru_data->run_flag = 0;
+				} else {
+					printf("The task has not started yet, please enter 1 to start PRU\n");
+				}
+				break;
+
+			case 3:
+				taskContinue = 0;
+				break;
+			default:
+				printf("Unknown input\n");
+		}
+	}
 	
-	// Wait for PRU0 to halt
-	prussdrv_pru_wait_event(PRU_EVTOUT_0);
-	prussdrv_pru_clear_event(PRU0_ARM_INTERRUPT);
-	
-	// Safe Quit PRUs
-	prussdrv_pru_disable(PRU_NUM0);
-	prussdrv_exit();
-	
+	if (workthread_running){
+		info.pru_data->run_flag = 0;
+	}
+
+	printf("Finished Execution\n\n");
+
 	return (0);
 }
